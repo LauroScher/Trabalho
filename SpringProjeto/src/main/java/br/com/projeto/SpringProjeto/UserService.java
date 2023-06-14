40bd4186e5b29d52d3fb1cb8ffb3d74cedeaa88b
LAURO SCHER <91957772+LauroScher@users.noreply.github.com>
2023-06-13 22:58:38 -0300

    Update UserService.java

--- SpringProjeto/src/main/java/br/com/projeto/SpringProjeto/UserService.java
+++ SpringProjeto/src/main/java/br/com/projeto/SpringProjeto/UserService.java
@@ -1,5 +1,44 @@
 package br.com.projeto.SpringProjeto;
 
-public class UserService {
+import org.springframework.stereotype.Service;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
 
+@Service
+public class UserService {
+    private static final Map<Long, User> request = new HashMap<>(); 
+    
+    public User criar(User user) {
+    	Long proximoId = request.keySet().size() + 1L;
+    	user.setId(proximoId);
+		return user;}
+    
+    public User atualizar(User user, Long userId) {
+    	user.put(userId, user);
+    	return user;
+    }
+    
+    public User getById (long userId) {
+    	return User.get(userId);
+    }
+    
+    public List<User> getAll () {
+    	return new ArrayList<>(User.values());
+    }
+    
+    public String delete(Long userId) {
+    	User.remove(userId);
+    	return "delete";
+    }
+
+	public UserService atualizar(UserService userservice, Long userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public UserService criar(UserService userservice) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
